8b30523d7c8f99e2d9ffcce566e8ae3194a19967
Soumyabrata Ghosh <soghosh@clarku.edu>
2025-03-20 00:57:18 +0000

    Added BrowserRouter to index.js for routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App /> {/* Removed BrowserRouter from here */}
+  <BrowserRouter> {/* Wrap the App component with BrowserRouter */}
+    <App />
+  </BrowserRouter>
 );
